1647c025b8048a331ae49181186a6a72f3fbd7f7
AhmedTarek22 <ahmedtarekabdo22@gmail.com>
2024-12-11 09:53:37 +0200

    Initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <div>
+      <h2>Restaurant</h2>
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>How to order</li>
+        <li>Popular dishes</li>
+        <li>Latest new</li>
+        <li>Offers</li>
+        <li>Contact us</li>
+      </ul>
+      <div>
+        <button>Login</button>
+        <button>Sign up</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
